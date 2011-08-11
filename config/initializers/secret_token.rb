4dc007d8769de607a250166d65f15287caa4243d
vu xuan tung <tungvx@tungvx-Aspire-4745.(none)>
2011-08-11 09:34:39 +0700

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Survey::Application.config.secret_token = '5bd8ba6b23a620e0a703defd635b239124b5297798ea2423d66e6d01a9ae7550496dabaf4cd71d3f5e8e7ccab2d8777d0cdcfa6c8a7cd537775eee1a070109ff'
