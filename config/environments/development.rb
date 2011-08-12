88649ca195c670efd901ef52545d75325b3f0a85
Vu Xuan Tung <toilatung90@gmail.com>
2011-08-13 02:42:21 +0700

    add configuration for devise gem using

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,8 @@ Survey::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  #devise gem configuration
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' } 
 end
 
