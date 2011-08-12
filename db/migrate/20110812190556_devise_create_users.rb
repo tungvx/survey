95390ee3ee6118955bfed8c977ef263f515a08ee
Vu Xuan Tung <toilatung90@gmail.com>
2011-08-13 02:30:19 +0700

    add user to devise

--- db/migrate/20110812190556_devise_create_users.rb
+++ db/migrate/20110812190556_devise_create_users.rb
@@ -2,7 +2,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
       t.database_authenticatable :null => false
-      t.confirmable
+      #t.confirmable
       t.recoverable
       t.rememberable
       t.trackable
@@ -12,7 +12,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
     end
 
     add_index :users, :email,                :unique => true
-    add_index :users, :confirmation_token,   :unique => true
+    #add_index :users, :confirmation_token,   :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :unlock_token,         :unique => true
   end
